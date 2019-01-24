e3753f283426d00ae9926a1f7234be019f049568
ilai <36520401+il41@users.noreply.github.com>
2019-01-24 12:19:31 -0600

    initcomit

---
+++ proxyServer.js
@@ -0,0 +1,12 @@
+// run your proxy server on port 80
+const proxy = require('redbird')({port: 80})
+// if someone visits ur ip address directly, reroute to the server running on 8000
+proxy.register("138.197.71.93", "http://localhost:8000")
+// if someone visits pizza.com also reroute that to port 8000
+proxy.register("ilai.link", "http://localhost:8000")
+// also helps to match the www subdomain as well
+proxy.register("www.ilai.link", "http://localhost:8000")
+// if someone visits poop.com reroute that to port 8001
+proxy.register("netjam.studio", "http://localhost:8001")
+proxy.register("www.netjam.studio", "http://localhost:8001")
+// etc...
