2b56d0c2237a93ab3d37d35c274550df661b9fe3
il41 <ilai.gilbert@gmail.com>
2019-04-10 17:42:09 -0500

    added capt.live

--- proxyServer.js
+++ proxyServer.js
@@ -1,7 +1,7 @@
 // run your proxy server on port 80
 const proxy = require('redbird')({port: 80})
 // if someone visits ur ip address directly, reroute to the server running on 8000
-proxy.register("138.197.71.93", "http://localhost:8002")
+proxy.register("138.197.71.93", "http://localhost:8000")
 // if someone visits pizza.com also reroute that to port 8000
 proxy.register("ilai.link", "http://localhost:8000")
 // also helps to match the www subdomain as well
@@ -10,3 +10,5 @@ proxy.register("www.ilai.link", "http://localhost:8000")
 proxy.register("netjam.studio", "http://localhost:8001")
 proxy.register("www.netjam.studio", "http://localhost:8001")
 // etc...
+proxy.register("capt.live", "http://localhost:8002")
+proxy.register("www.capt.live", "http://localhost:8002")
